76f922e543fbdee0d97f41e1a8b5a9396aed3d3b
harshalambhoremech <100512258+harshalambhoremech@users.noreply.github.com>
2022-04-08 04:39:41 -0700

    Update Homepage.java
    i just add one line in that please check it and use it.

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 public class Homepage {
 	@FindBy(xpath = "//*[ @id='branding']//a[@id='welcome']")
 	private WebElement meanu;
-	
+	//I am a tester
 	@FindBy(xpath="//*[@id='welcome-menu']/ul/li[3]/a[text()='Logout']")
 	private WebElement logout;
 	
